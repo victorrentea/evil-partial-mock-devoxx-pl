b0f4a50ac0df4f45b762ed54e964c4c2be17c29e
Victor Rentea <victorrentea@gmail.com>
2017-06-20 11:57:06 +0300

    rename test to canPlaceAnOrder

--- src/test/java/victor/OrderServiceTest.java
+++ src/test/java/victor/OrderServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class OrderServiceTest {
 
 	@Test
-	public void dummy() {
+	public void canPlaceAnOrder() {
 		OrderService service = new OrderService();
 		Order order = new Order();
 		service.placeOrder(order);
