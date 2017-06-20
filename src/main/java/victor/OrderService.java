f5a2eac3f55dfa5ffdd7ca19dea17da2a4a83872
Victor Rentea <victorrentea@gmail.com>
2017-06-20 12:05:23 +0300

    refactor some more

--- src/main/java/victor/OrderService.java
+++ src/main/java/victor/OrderService.java
@@ -2,14 +2,11 @@ package victor;
 
 public class OrderService {
 
+	// @Inject/@Autowire
 	private OrderRepository repo;
 
 	public void placeOrder(Order order) {
 		repo.save(order);
-	}
-
-	public void setOrderRepository(OrderRepository repo) {
-		this.repo = repo;
 	}
 
 }
