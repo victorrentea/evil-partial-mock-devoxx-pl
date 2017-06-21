f7bcd8fe7ca3152566fe8d24f4ee9bdbdd5317d9
Victor Rentea <victorrentea@gmail.com>
2017-06-21 14:22:29 +0200

    end of quickie

--- src/test/java/victor/OrderServiceTest.java
+++ src/test/java/victor/OrderServiceTest.java
@@ -3,14 +3,10 @@ package victor;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
@@ -27,20 +23,18 @@ public class OrderServiceTest {
 	private OrderRepository orderRepo;
 
 	@Mock
-	private EmailClient emailClient;
+	private EmailService emailService;
 	
 	private Order order = new Order();
 
 	@Test
 	public void placeOrder_peristsTheOrder() {
-		order.setCustomerName("Victor");
 		service.placeOrder(order);
 		verify(orderRepo).save(order);
 	}
 	
 	@Test
 	public void placeOrder_returnsConfirmationNumber() {
-		order.setCustomerName("Victor");
 		String confirmation = service.placeOrder(order);
 		assertEquals(confirmation, order.getConfirmationNumber());
 		assertNotNull(confirmation);
@@ -48,21 +42,21 @@ public class OrderServiceTest {
 	
 	@Test
 	public void placeOrder_sendsConfirmationEmail() {
-		order.setCustomerName("Victor");
 		service.placeOrder(order);
-		ArgumentCaptor<Email> email = ArgumentCaptor.forClass(Email.class);
-		verify(emailClient).send(email.capture());
-		assertEquals("Order Received", email.getValue().getSubject());
-		assertEquals("Thank you, VICTOR", email.getValue().getBody());
+		verify(emailService).sendEmail(order, "Order Received");
+//		
+		//TODO vrentea: move to EmailServiceTest
+//		ArgumentCaptor<Email> email = ArgumentCaptor.forClass(Email.class);
+//		verify(emailClient).send(email.capture());
+//		assertEquals("Order Received", email.getValue().getSubject());
+//		assertEquals("Thank you, VICTOR", email.getValue().getBody());
 	}
 	
 	@Test
 	public void shipOrder_ok() {
-		doNothing().when(service).sendEmail(anyObject(), anyString());
-		
 		service.shipOrder(order);
 		assertTrue(order.isShipped());
 		
-		verify(service).sendEmail(order, "Order Shipped");
+		verify(emailService).sendEmail(order, "Order Shipped");
 	}
 }
